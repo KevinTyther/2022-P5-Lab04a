f40081155e8cb33cc92ab31812c19d3b6f622ea0
Kevin <kevin.tyther@vtsd.com>
2022-09-30 13:10:55 -0400

    90%

--- Lab04avst.java
+++ Lab04avst.java
@@ -7,20 +7,41 @@ public class Lab04avst
 {
     public static void main(String[] args)
     {
-        System.out.println("Lab04a, Student Version\n");
+        System.out.println("Lab04a, 90 Point Version\n");
 
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
         double numMonth = 30 * 12;
-        double RoI = 0.0575; // Rate of Interest
+        double RoI = (annualRate / 100) / 12; //Rate of interest
         double top;
         double bottom;
         double monthly;
+        double totalPayment;
+        double totalInterest;
 
         top = 1 + RoI;
         top = Math.pow(top,numMonth);
         top = RoI * top;
+        bottom = 1 + RoI;
+        bottom = Math.pow(bottom,numMonth);
+        bottom = bottom - 1;
+        monthly = top / bottom;
+        monthly = monthly * principal;
+
+        totalPayment = monthly * numMonth;
+        totalInterest = totalPayment - principal;
+
+        monthly = (double)(Math.round(monthly * 100)) / 100;
+        totalPayment = (double)((Math.round(totalPayment * 100)) / 100);
+
+
+        System.out.println("Principal:" + "\t\t\t" + principal);
+        System.out.println("Annual Rate:" + "\t\t" + annualRate +"%");
+        System.out.println("Number of Years:" + "\t" + numYears);
+        System.out.println("Monthly Payment:" + "\t" + monthly);
+        System.out.println("Total Payment:" + "\t\t" + totalPayment);
+        System.out.println("Total Interest:" + "\t\t" + totalInterest);
 
 
 
